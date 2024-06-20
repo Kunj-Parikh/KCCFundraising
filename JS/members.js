c6921f31d33f4ca43e2996f1388e0e659c296216
bz421 <bozhan.harry@gmail.com>
2024-06-19 20:51:02 -0400

    changed bg colors, added bozhan to members, removed gap below footer, added about us animation, fixed typos

---
+++ JS/members.js
@@ -0,0 +1,10 @@
+const img = document.querySelector('#labcoats')
+
+window.addEventListener('scroll', function() {
+    const FACTOR = -0.000075
+    console.log('scrolling: ' + scrollY)
+    if (scrollY * (FACTOR) + 1 < 0 || scrollY * (FACTOR) + 1 > 1) return
+    img.setAttribute('style', `transform: scale(${scrollY*(FACTOR)+1}); width: 100%`)
+    
+})
+
