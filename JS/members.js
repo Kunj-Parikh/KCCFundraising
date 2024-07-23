c92e09ffb41e593afc7fd5ef993e00c85c2b59c0
Kunj Parikh <9002birdwatcher@gmail.com>
2024-07-23 17:26:33 -0400

    Removed labcoat image

--- JS/members.js
+++ JS/members.js
@@ -1,9 +1,9 @@
-const img = document.querySelector('#labcoats')
+// const img = document.querySelector('#labcoats')
 
-window.addEventListener('scroll', function() {
-    const FACTOR = -0.00055
-    console.log('scrolling: ' + scrollY)
-    if (scrollY * (FACTOR) + 1 < 0 || scrollY * (FACTOR) + 1 > 1) return
-    img.setAttribute('style', `transform: scale(${scrollY*(FACTOR)+1}); width: 100%`)
+// window.addEventListener('scroll', function() {
+//     const FACTOR = -0.00055
+//     console.log('scrolling: ' + scrollY)
+//     if (scrollY * (FACTOR) + 1 < 0 || scrollY * (FACTOR) + 1 > 1) return
+//     img.setAttribute('style', `transform: scale(${scrollY*(FACTOR)+1}); width: 100%`)
 
-})
+// })
